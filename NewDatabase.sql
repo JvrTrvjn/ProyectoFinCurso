d94f9ed22291d427ba80a8ee36419a4d75e32567
Javier Trevijano <javier.trevijano@hotmail.com>
2023-03-01 21:19:31 +0100

    insertando datos en la BD

--- NewDatabase.sql
+++ NewDatabase.sql
@@ -14,6 +14,14 @@ ALTER TABLE pedidos modify column PedidoID int auto_increment;
 ALTER TABLE libros modify column LibroID int auto_increment;
 ALTER TABLE Usuarios modify column UsuarioID int auto_increment;
 ALTER TABLE Pedidos add foreign key(LibroID) references libros(LibroID), add foreign key(UsuarioID) references Usuarios(UsuarioID);
+INSERT INTO Libros VALUES (1,'El Quijote','Miguel de Cervantes','12096754', 20);
+INSERT INTO Libros VALUES (2,'El vieto nos llevará','Monica Carrillo','12096734', 17);
+INSERT INTO Libros VALUES (3,'Azami','Aki Shimazaku','12096712', 15);
+INSERT INTO Libros VALUES (4,'La huella de infierno','Leigh Bardugo','12096721', 18);
+INSERT INTO Libros VALUES (5,'El cuco de cristal','Jvier Castillo','12096743', 21);
+INSERT INTO Libros VALUES (1,'1','Administrador','Admin', 'javier.trevijano@outlook.com');
+INSERT INTO Libros VALUES (2,'12345678','Juan','Campanario', 'juanito@gmail.com');
+
 
 
 
