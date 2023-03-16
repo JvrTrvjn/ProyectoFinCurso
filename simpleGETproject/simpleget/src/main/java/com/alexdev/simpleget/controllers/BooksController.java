aafa22eec246a6175b8a6d8c31781717f9aa4448
Javier Trevijano <javier.trevijano@hotmail.com>
2023-03-16 10:02:22 +0100

    "Put Method (Builder)"

--- simpleGETproject/simpleget/src/main/java/com/alexdev/simpleget/controllers/BooksController.java
+++ simpleGETproject/simpleget/src/main/java/com/alexdev/simpleget/controllers/BooksController.java
@@ -2,15 +2,11 @@ package com.alexdev.simpleget.controllers;
 
 import com.alexdev.simpleget.repositorys.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.alexdev.simpleget.services.BookService;
-
 import com.alexdev.simpleget.entity.Book;
-import org.springframework.web.servlet.support.JstlUtils;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
@@ -18,8 +14,6 @@ public class BooksController {
 
     @Autowired
     private BookService bookService;
-	@Autowired
-	private BookRepository bookRepository;
 
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
@@ -42,34 +36,14 @@ public class BooksController {
 	}
 	@PutMapping(value ="/books/{id}")
 	public void update (@RequestBody Book updateBook, @PathVariable Long id) {
-
-		bookService.save(Book.builder().id(id).title(updateBook.getTitle())
-				.author(updateBook.getAuthor()).price(updateBook.getPrice())
+		bookService.save(Book.builder()
+				.id(id)
+				.title(updateBook.getTitle())
+				.author(updateBook.getAuthor())
+				.price(updateBook.getPrice())
 				.ISBN(updateBook.getISBN()).build());
 
-
-		/*Optional<Book> bookOptional = bookService.getBook(id);
-		if(bookOptional.isEmpty())
-			return ResponseEntity.notFound().build();
-		updateBook.setId(id);
-		bookService.save(updateBook);
-		return  ResponseEntity.noContent().build();
-	/*public Book update(@RequestBody Book updateBook, @PathVariable Long id){
-		Book upBook = bookService.getBook(id);
-		upBook.setTitle(updateBook.getTitle());
-		upBook.setAuthor(updateBook.getAuthor());
-		upBook.setPrice(updateBook.getPrice());
-		upBook.setISBN(updateBook.getISBN());
-		bookService.save(upBook);
-		return upBook;*/
 	}
-	/*@PutMapping(value ="/books/{id}")
-	public void updateBook(@RequestBody Book newBook,@PathVariable Long id){
-		if(id.equals(newBook.getId())){
-			bookService.delete(id);
-			bookService.save(newBook);}
-		else {
-			bookService.save(newBook);}
-	}*/
+
 
 }
